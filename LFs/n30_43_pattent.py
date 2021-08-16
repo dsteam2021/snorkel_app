000e6d45396632b8b361825c85cb586e037111d4
unknown <hanghust98@gmail.com>
2021-08-16 15:00:20 +0700

    update code

--- LFs/n30_43_pattent.py
+++ LFs/n30_43_pattent.py
@@ -14,8 +14,9 @@ df_pair3 = pd.read_csv("\Pattent_data\pair_word_apriori_3_hangnt.csv")
 
 df_pair1['items'] = df_pair1['items'].swifter.apply(convert_stringlist_to_list)
 df_pair2['items'] = df_pair2['items'].swifter.apply(convert_stringlist_to_list)
+df_pair3['items'] = df_pair3['items'].swifter.apply(convert_stringlist_to_list)
 
-df_pair1 = pd.concat([df_pair1, df_pair2], ignore_index=True)
+df_pair1 = pd.concat([df_pair1, df_pair2, df_pair3], ignore_index=True)
 
 @labeling_function()
 def pattent_for_career_1(x):
