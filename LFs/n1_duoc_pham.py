be480acae3cb0f898866a26461252e65a8f18958
Tran Huy <tranhuuhuy297@gmail.com>
2021-08-16 17:59:09 +0700

    fix lf duoc pham

--- LFs/n1_duoc_pham.py
+++ LFs/n1_duoc_pham.py
@@ -8,21 +8,20 @@ def keyword_1(x):
     try:
         if x['check_btype'] == 'Dược phẩm':
             keyword = ['thuốc', 'dược']
-            keyword.extend(convert(keyword))
             for i in keyword:
-                if (i in x['name_cleaned']) or (i in x['company_name']): 
+                if i in x['name_cleaned']: 
                     return 1
-
-            don_vi = ['ml', 'l', 'lít', 'lit', 'mg', 'g', 'kg']
-            for i in don_vi:
-                if i in x['name_cleaned'].split(): 
-                    return 1
-                for j in x['name_cleaned'].split():
-                    if re.search('[0-9].*' + 'i', j):
-                        return 1
 
         elif x['check_btype'] != 'Dược phẩm':
             pass
+            
+        don_vi = ['ml', 'l', 'lít', 'lit', 'mg', 'g', 'kg']
+        for i in don_vi:
+            if i in x['name'].split(): 
+                return 1
+            if re.search('[0-9].' + i, x['name']):
+                return 1
+
     except: 
         # print(x)
         pass
