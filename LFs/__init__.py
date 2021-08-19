8ecaa22e279ea49d9f58c8776c5a07b07af7feb2
Tran Huy <tranhuuhuy297@gmail.com>
2021-08-19 11:49:24 +0700

    add save dât

--- LFs/__init__.py
+++ LFs/__init__.py
@@ -12,6 +12,6 @@ lfs = []
 # n0_bat_dong_san.py -> n0_bat_dong_san
 # lọc các file lfs để auto import
 list_lfs = [i.split('.')[0] for i in os.listdir('LFs') if (i != 'util.py' and i[0] != '_' and i.split('.')[-1] == 'py')]
-for i in list_lfs[:5]:
+for i in list_lfs:
     file_lfs = __import__('LFs.' + i, fromlist=['get_lfs'])
     lfs.extend(file_lfs.get_lfs())
