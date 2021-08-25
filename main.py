38db5d5638469dc1f7946f302bc56e1178290b1b
tranhuuhuy297 <46334604+tranhuuhuy297@users.noreply.github.com>
2021-08-25 17:16:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,60 +14,32 @@ from snorkel.labeling.model import MajorityLabelVoter
 
 
 if __name__ == "__main__":
-    def get_L(args, df, lfs):
-        """
-        Args:
-            args:
-            df: (pd.DataFrame) data
-            lfs: (list) list labeling function get from LFS/__init__.py
-        Return:
-            L_train: (np.array) output of labeling function, has shape (n, m): (data point, number labeling functions)
-            L_analyst: (pd.DataFrame) 
-        """
-        if args.core == 1: 
-            # lfs: List  các labeling function ở LFs
-            applier = PandasLFApplier(lfs=lfs)
-            L_train = applier.apply(df=df)
-        elif args.core > 1: 
-            applier = PandasParallelLFApplier(lfs)
-            L_train = applier.apply(df=df, n_parallel=args.core)
-
-        L_analyst = LFAnalysis(L=L_train, lfs=lfs).lf_summary()
-
-        return L_train, L_analyst
-    
-    def get_label(args, num_of_label, L_train):
-        """
-        Args:
-            args:
-            num_of_label: (int) number labels
-            L_train: (np.array) output of labeling function, has shape (n, m): (data point, number labeling functions)
-        Return:
-            y_preds: (np.array) predicted output by Snorkel after get L_train, has shape (data point, )
-        """
-        # Dùng major vote hoặc label_model để tìm nhãn
-        if args.major_vote:
-            majority_model = MajorityLabelVoter()
-            y_preds = majority_model.predict(L=L_train)
-        elif args.label_model:
-            label_model = LabelModel(cardinality=num_of_label, verbose=True)
-            label_model.fit(L_train=L_train, n_epochs=args.epoch, log_freq=100, seed=args.seed)
-            y_preds = label_model.predict(L=L_train)
-        else:
-            label_model = LabelModel(cardinality=num_of_label, verbose=True)
-            label_model.fit(L_train=L_train, n_epochs=args.epoch, log_freq=100, seed=args.seed)
-            y_preds = label_model.predict(L=L_train)
-
-        return y_preds
-    
     args = get_args()
     start = time.time()
-
+    
     df, num_of_label, dict_temp = load_data(args)
 
-    L_train, L_analyst = get_L(args, df, lfs)
+    if args.core == 1: 
+        # lfs: List  các labeling function ở LFs
+        applier = PandasLFApplier(lfs=lfs)
+        L_train = applier.apply(df=df)
+    elif args.core > 1: 
+        applier = PandasParallelLFApplier(lfs)
+        L_train = applier.apply(df=df, n_parallel=args.core)
+
+    L_analyst = LFAnalysis(L=L_train, lfs=lfs).lf_summary()
     
-    y_preds = get_label(args, num_of_label, L_train)
+    if args.major_vote:
+        majority_model = MajorityLabelVoter()
+        y_preds = majority_model.predict(L=L_train)
+    elif args.label_model:
+        label_model = LabelModel(cardinality=num_of_label, verbose=True)
+        label_model.fit(L_train=L_train, n_epochs=args.epoch, log_freq=100, seed=args.seed)
+        y_preds = label_model.predict(L=L_train)
+    else:
+        label_model = LabelModel(cardinality=num_of_label, verbose=True)
+        label_model.fit(L_train=L_train, n_epochs=args.epoch, log_freq=100, seed=args.seed)
+        y_preds = label_model.predict(L=L_train)
 
     print("Time consuming: {}".format(time.time() - start))
 
